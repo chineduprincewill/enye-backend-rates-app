a49c88ef84c509d2699a2838b3dab47e716d50e8
chineduprincewill <chinedu.ijeomah@yahoo.com>
2021-01-21 11:39:10 +0100

    updated welcome message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app =  express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const welcome = '<h3>Welcome to Enye 2021 Backend Task 1</h3><p>Click <a href="https://enye-rates-app.herokuapp.com/api/rates/base=USD/currency=EUR,CZK,GBP">here</a> to check your rates</p><p>You can modify the <b>base</b> and <b>currency</b> parameters in the url to check your rates</p><p>Thanks!</p>';
+const welcome = '<h3>Welcome to Enye 2021 Backend Task 1</h3><p>Click <a href="https://enye-rates-app.herokuapp.com/api/rates/base=USD/currency=EUR,CZK,GBP">here</a> or copy this link <b>https://enye-rates-app.herokuapp.com/api/rates/base=USD/currency=EUR,CZK,GBP</b> and make a GET request on Postman to check your rates</p><p>You can modify the <b>base</b> and <b>currency</b> parameters in the url to check your rates</p><p>Thanks!</p>';
 
 app.get('/', (req, res) => res.send(welcome));
 
