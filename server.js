8298d24dcdcbf6d7243d51226f8fe06d2aae38b6
chineduprincewill <chinedu.ijeomah@yahoo.com>
2021-01-21 11:12:36 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use('/api/rates', rates);
 
-//const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-//app.listen(port, () => console.log(`Server running on port ${port}`));
-
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(port, () => console.log(`Server running on port ${port}`));
